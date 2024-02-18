5c02ba777d8332b90a4f71cbb1447f6f7fefa807
Ethan <eman5797@gmail.com>
2024-02-17 22:49:51 -0500

    added cssbaseline

--- bruces-bday-site/src/pages/_app.js
+++ bruces-bday-site/src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { CssBaseline } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline /> {/* CssBaseline can be removed to go back to default Nextjs Styles */}
+      <Component {...pageProps} />
+    </>
+  );
 }
--- bruces-bday-site/src/pages/index.js
+++ bruces-bday-site/src/pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
